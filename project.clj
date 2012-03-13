d26a9b31bc185711db611a8ef9972991f1b63921
wuzhenzhou <zzwu@adminmatoMacBook-Pro-4.local>
2012-03-13 16:24:58 +0800

    init

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject todo-list "1.0.0-SNAPSHOT"
   :description "A simple blog comments system"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [postgresql/postgresql "9.0-801.jdbc4"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]
                  [org.clojure/java.jdbc "0.0.3"]
                  [ring/ring-jetty-adapter "0.3.10"]
                  [compojure "0.6.4"]
