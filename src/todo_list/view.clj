000e3ec415b7dcbc69224a346e345fec6d88ec27
wuzhenzhou <zzwu@adminmatoMacBook-Pro-4.local>
2012-03-14 13:58:13 +0800

    add ignore files

--- src/todo_list/view.clj
+++ src/todo_list/view.clj
@@ -1,6 +1,6 @@
 (ns todo-list.view
   (:use [hiccup.core :only [html escape-html]]
-        [hiccup.form-helpers :only [form-to label text-area text-field submit-button]])
+        [hiccup.form-helpers :only [form-to label text-field submit-button]])
   (:require [todo-list.layout :as layout]))
 
 (defn note-display
